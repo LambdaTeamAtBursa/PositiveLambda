a951f3791b00b2ed5edab31a124e733d16457b49
Roman Gural <7.gural@gmail.com>
2014-12-25 23:01:52 +0200

    deleted unnecessary timestamp field from relational table

--- db/migrate/20141225102109_add_tags_to_cyte.rb
+++ db/migrate/20141225102109_add_tags_to_cyte.rb
@@ -3,13 +3,6 @@ class AddTagsToCyte < ActiveRecord::Migration
     create_table :tags_cytes, id:false do |t|
       t.integer :tag_id, index: true, null:false
       t.integer :cyte_id, index: true, null:false
-
-      t.timestamps null:false
     end
-
-    #add_index :tags_cytes, :tag_id
-    #add_index :tags_cytes, :cyte_id
-
-    #add_reference :cytes, :tags, index: true
   end
 end
