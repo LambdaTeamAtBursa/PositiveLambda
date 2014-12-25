fdb462f2273127f864912515b1d1044295deaaa9
Roman Gural <7.gural@gmail.com>
2014-12-25 10:08:14 +0200

    changed faker option for cyte

--- spec/factories/cyte_categories.rb
+++ spec/factories/cyte_categories.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
     require 'faker'
   factory :cyte_category do
-    name { Faker::Lorem.characters(rand(4..30)) }
+    name { Faker::Lorem.sentence }
     
     factory :cyte_with_parent_category do
       association :parent_category, factory: :cyte_category
