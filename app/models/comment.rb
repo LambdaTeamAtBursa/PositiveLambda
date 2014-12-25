334de40fbfe9d5b02d23a2121a2d5637efd0a46d
Roman Gural <7.gural@gmail.com>
2014-12-25 23:22:18 +0200

    moved dependent option for comments to models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
-  belongs_to :cyte
-  belongs_to :parent_comment, class_name: Comment
+  belongs_to :cyte, dependent: :delete
+  belongs_to :parent_comment, class_name: Comment, dependent: :delete
   has_many :comments, foreign_key: :parent_comment_id
 end
--- db/migrate/20141224124625_create_comments.rb
+++ db/migrate/20141224124625_create_comments.rb
@@ -2,8 +2,8 @@ class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
       t.belongs_to :user, index: true
-      t.belongs_to :cyte, index: true, dependent: :delete
-      t.belongs_to :parent_comment, class_name: Comment, index: true, dependent: :delete
+      t.belongs_to :cyte, index: true
+      t.belongs_to :parent_comment, class_name: Comment, index: true
       t.text :text, null: false
 
       t.timestamps null: false
