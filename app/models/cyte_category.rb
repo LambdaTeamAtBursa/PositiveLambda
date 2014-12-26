85cf33e78d02ceb76e7df3d30dfcf99538e8f158
Roman Gural <7.Silvan@gmail.com>
2014-12-26 11:17:42 +0200

    added dependent: :delete to cyte_category

--- app/models/cyte_category.rb
+++ app/models/cyte_category.rb
@@ -1,4 +1,4 @@
 class CyteCategory < ActiveRecord::Base
   has_many :cyte_categories, foreign_key: :parent_category_id
-  belongs_to :parent_category, class_name: CyteCategory
+  belongs_to :parent_category, class_name: CyteCategory, dependent: :delete
 end
