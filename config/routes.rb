0590a66c052a1f8bd79c40339bcff38676c41aec
Roman Gural <7.gural@gmail.com>
2014-12-29 14:14:02 +0200

    deleted welcome controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,7 @@ Rails.application.routes.draw do
   end
 
   devise_for :users
-  get 'welcome/index'
-  #root 'welcome#index'
-
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
