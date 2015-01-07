91fc2d8d14da2e27c03a42dc98245424d73fcf55
Kinnarts <an.zhirkov@gmail.com>
2015-01-08 01:03:28 +0300

    issue_23 without pagination

--- app/controllers/cyte_categories_controller.rb
+++ app/controllers/cyte_categories_controller.rb
@@ -1,11 +1,52 @@
 class CyteCategoriesController < ApplicationController
-  
+  before_action :find_cyte_category, only: [:show, :edit, :update, :destroy]
+
   def index
     @cyte_categories = CyteCategory.all
   end
 
   def show
+  end
+
+  def new
+    @cyte_category = CyteCategory.new
+  end
+
+  def create
+    @cyte_category = CyteCategory.new(cyte_category_params)
+    if @cyte_category.save
+      redirect_to cyte_categories_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @cyte_category.update(cyte_category_params)
+      redirect_to cyte_categories_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @cyte_category.destroy
+    redirect_to cyte_categories_path
+  end
+
+
+  private
+
+  def find_cyte_category
     @cyte_category = CyteCategory.find(params[:id])
   end
+
+  def cyte_category_params
+    params.require(:cyte_category).permit(:name)
+  end
+
 
 end
--- spec/controllers/cyte_categories_controller_spec.rb
+++ spec/controllers/cyte_categories_controller_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe CyteCategoriesController, :type => :controller do
 
   context "when user logged in" do
     let(:user) { FactoryGirl.create(:user) }
-    subject { FactoryGirl.create(:cyte_with_parent_category) }
+    subject { FactoryGirl.create(:cyte_category) }
 
     before do
       sign_in user
@@ -27,9 +27,92 @@ RSpec.describe CyteCategoriesController, :type => :controller do
 
       it "assigns the requested cyte_category to subject" do
         get :index
-        expect(assigns(:cyte_with_parent_category)).to eq([subject])
+        expect(assigns(:cyte_categories)).to eq([subject])
       end
     end
+
+    describe "GET #show" do
+      it "assigns the requested cyte_category to subject" do
+        get :show, id: subject
+        expect(assigns(:cyte_category)).to eq(subject)
+      end
+
+      it "renders the :show view" do
+        get :show, id: subject
+        expect(response).to render_template :show
+      end
+    end
+
+    describe "GET #new" do
+      it "assigns the requested cyte_category to new cyte_category" do
+        get :new
+        expect(assigns(:cyte_category)).to be_new_record
+      end
+
+      it "renders the :new view" do
+        get :new
+        expect(response).to render_template :new
+      end
+    end
+
+    describe "POST #create" do
+      context "with valid attributes" do
+        it "creates new object" do
+          expect{
+            post :create, cyte_category: FactoryGirl.attributes_for(:cyte_category)
+          }.to change(CyteCategory, :count).by(1)
+        end
+
+        it "redirects to index path" do
+          post :create, cyte_category: FactoryGirl.attributes_for(:cyte_category)
+          expect(response).to redirect_to cyte_categories_path
+        end
+      end
+    end
+
+    describe "GET #edit" do
+      it "assigns the requested cyte_category to subject" do
+        get :edit, id: subject
+        expect(assigns(:cyte_category)).to eq(subject)
+      end
+
+      it "renders the :edit view" do
+        get :edit, id: subject
+        expect(response).to render_template :edit
+      end
+    end
+
+    describe "PATCH #update" do
+      context "with valid attributes" do
+        it "updates object" do
+          expect{
+            post :update, id: subject, cyte_category: { name: 'new_category' }
+          }.to change{ subject.reload.name }.to('new_category')
+        end
+
+        it "redirects to index path" do
+          post :update, id: subject, cyte_category: { name: 'new_category' }
+          expect(response).to redirect_to cyte_categories_path
+        end
+      end
+    end
+
+    describe 'DELETE #destroy' do
+      before(:each) { @cyte_category = FactoryGirl.create :cyte_category}
+
+      it "deletes the cyte_category" do
+        expect {
+          delete :destroy, id: @cyte_category
+        }.to change(CyteCategory, :count).by(-1)
+      end
+
+      it "redirects to cyte_categories#index" do
+        delete :destroy, id: @cyte_category
+        expect(response).to redirect_to cyte_categories_path
+      end
+    end
+
+
   end
 
 end
