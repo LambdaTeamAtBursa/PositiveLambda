a2ca75742585002b99c44bc787023bdd4ad712bf
Vadim <vadim-ip@mail.ru>
2015-01-11 21:00:09 +0200

    ok rspec for home page

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -5,10 +5,13 @@ RSpec.describe HomeController, :type => :controller do
   describe "GET index" do
 
     let(:user) { FactoryGirl.create(:user) }
-    load Rails.root.join('db','seeds.db')
 
     before do
       sign_in user
+      # load Rails.root.join('db','seeds.db')
+      #  cannot load such file -- /srv/bursa/ror/PositiveLambda/db/seeds.db (LoadError)
+      #  неясная ошибка
+      FactoryGirl.create(:cyte)
     end
     
     it "returns http success" do
